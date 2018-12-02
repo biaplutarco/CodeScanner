f896fb6c35ccf15a0b539d2e59b2678ddfbf9d1d
Beatriz Plutarco <biaplutarco@gmail.com>
2018-12-02 20:00:56 -0300

    Initial Commit

---
+++ CodeScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeScanner
+//
+//  Created by Bia Plutarco on 02/12/18.
+//  Copyright © 2018 Bia Plutarco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
